d6d6ece74a91435df627452f93f9f897a29ec0e0
Bert Cotton <bert.cotton@gmail.com>
2016-08-27 15:26:10 -0600

    Fixing the middleware to look for the certificate correctly

--- src/TfsAdvanced/Infrastructure/ClientCertificateMiddleware.cs
+++ src/TfsAdvanced/Infrastructure/ClientCertificateMiddleware.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Diagnostics;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
@@ -12,73 +13,72 @@ namespace TfsAdvanced.Infrastructure
     public class ClientCertificateMiddleware
     {
         private readonly RequestDelegate _next;
-        private readonly CertificateValidation _config;
-        private readonly ILogger _logger;
+        private readonly AppSettings appSettings;
 
-        public ClientCertificateMiddleware(RequestDelegate next, ILoggerFactory loggerFactory, IOptions<AppSettings> options)
+        public ClientCertificateMiddleware(RequestDelegate next, IOptions<AppSettings> options)
         {
             _next = next;
-            _config = options.Value.CertificateValidation;
-            _logger = loggerFactory.CreateLogger<ClientCertificateMiddleware>();
+            appSettings = options.Value;
         }
 
         public async Task Invoke(HttpContext context)
         {
             //Validate the cert here
-
-            bool isValidCert = false;
-            X509Certificate2 certificate = null;
-
-            string certHeader = context.Request.Headers["X-ARR-ClientCert"];
-
-            if (!String.IsNullOrEmpty(certHeader))
+            try
             {
-                try
-                {
-                    byte[] clientCertBytes = Convert.FromBase64String(certHeader);
-                    certificate = new X509Certificate2(clientCertBytes);
 
-                    isValidCert = IsValidClientCertificate(certificate);
-                    if (isValidCert)
+                bool isValidCert = false;
+                X509Certificate2 certificate = null;
+
+                string certHeader = context.Request.Headers["X-ARR-ClientCert"];
+                if (String.IsNullOrEmpty(certHeader))
+                    certHeader = context.Request.Headers["MS-ASPNETCORE-CLIENTCERT"];
+
+                if (!String.IsNullOrEmpty(certHeader))
+                {
+                    try
                     {
-                        //Invoke the next middleware in the pipeline
-                        await _next.Invoke(context);
+                        byte[] clientCertBytes = Convert.FromBase64String(certHeader);
+                        certificate = new X509Certificate2(clientCertBytes);
+
+                        isValidCert = IsValidClientCertificate(certificate);
+                        if (isValidCert)
+                        {
+                            //Invoke the next middleware in the pipeline
+                            await _next.Invoke(context);
+                        }
+                        else
+                        {
+                            //Stop the pipeline here.
+                            Debug.WriteLine("Certificate is not valid");
+                 //           context.Response.StatusCode = 403;
+                        }
                     }
-                    else
+                    catch (Exception ex)
                     {
-                        //Stop the pipeline here.
-                        _logger.LogError("Certificate is not valid");
+                        //What to do with exceptions in middleware?
+                        Debug.WriteLine(ex.Message, ex);
+                        await context.Response.WriteAsync(ex.Message);
                         context.Response.StatusCode = 403;
                     }
                 }
-                catch (Exception ex)
+                else
                 {
-                    //What to do with exceptions in middleware?
-                    _logger.LogError(ex.Message, ex);
-                    await context.Response.WriteAsync(ex.Message);
-                    context.Response.StatusCode = 403;
+                    Debug.WriteLine("X-ARR-ClientCert header is missing");
+              //      context.Response.StatusCode = 403;
                 }
             }
-            else
+            catch (Exception ex)
             {
-                _logger.LogError("X-ARR-ClientCert header is missing");
-                context.Response.StatusCode = 403;
+                context.Response.Headers["Error"] = ex.Message;
+            //    context.Response.StatusCode = 400;
             }
         }
 
 
         private bool IsValidClientCertificate(X509Certificate2 certificate)
         {
-            // In this example we will only accept the certificate as a valid certificate if all the conditions below are met:
-            // 1. The certificate is not expired and is active for the current time on server.
-            // 2. The subject name of the certificate has the common name nildevecc
-            // 3. The issuer name of the certificate has the common name nildevecc and organization name Microsoft Corp
-            // 4. The thumbprint of the certificate is 30757A2E831977D8BD9C8496E4C99AB26CB9622B
-            //
-            // This example does NOT test that this certificate is chained to a Trusted Root Authority (or revoked) on the server
-            // and it allows for self signed certificates
-            //
-
+            var _config = appSettings.CertificateValidation;
             if (null == certificate) return false;
 
             // 1. Check time validity of certificate
@@ -135,18 +135,5 @@ namespace TfsAdvanced.Infrastructure
 
     }
 
-    // Extension method used to add the middleware to the HTTP request pipeline.
-    public static class ClientCertMiddlewareExtensions
-    {
-        public static IApplicationBuilder UseClientCertMiddleware(this IApplicationBuilder builder)
-        {
-            return builder.UseMiddleware<ClientCertificateMiddleware>();
-        }
-
-        public static IApplicationBuilder UseClientCertMiddleware(this IApplicationBuilder builder, IOptions<CertificateValidation> options)
-        {
-
-            return builder.UseMiddleware<ClientCertificateMiddleware>(options);
-        }
-    }
+   
 }
