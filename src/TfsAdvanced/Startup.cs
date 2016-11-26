9967d9b6dd99f4dc36cb69cda33e5e80c23e5739
Robert Cotton <Robert.Cotton@labor.idaho.gov>
2016-11-26 09:07:38 -0700

    fixing the build

--- src/TfsAdvanced/Startup.cs
+++ src/TfsAdvanced/Startup.cs
@@ -39,7 +39,7 @@ namespace TfsAdvanced
 
             builder.AddApplicationInsightsSettings(developerMode: true);
 
-            Configuration = builder.Build()
+            Configuration = builder.Build();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
