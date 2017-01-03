fcc49e533833661b087354560be003769dfefafc
Rocotton <RobertCotton@ius.gov>
2017-01-03 11:57:02 -0700

    Adding css and js and updating notifications

---
+++ src/TfsAdvanced/wwwroot/js/app.js
@@ -0,0 +1,10 @@
+﻿function formatPage() {
+    var longDateFormat = 'dd/MM/yyyy hh:mm:ss a';
+
+    $(".datetime").each(function (idx, elem) {
+        $(elem).text($.format.date($(elem).text(), longDateFormat));
+    });
+    $(".datetime-pretty").each(function (idx, elem) {
+        $(elem).text($.format.prettyDate($(elem).text()));
+    });
+}
